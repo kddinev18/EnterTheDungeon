7541400f11cf829af7378c1c858239a0bad2b869
kddinev18 <kddinev18@codingburgas.bg>
2022-04-23 16:08:54 +0300

    Add column constraints

--- EnterTheDungeon/Data/Models/User.cs
+++ EnterTheDungeon/Data/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,20 @@ namespace EnterTheDungeon.Data.Models
     public class User
     {
         public int Id { get; set; }
+
+        [MaxLength(128)]
+        [Required]
         public string Username { get; set; }
+
+        [MaxLength(128)]
+        [Required]
         public string Email { get; set; }
+
+        [MaxLength(128)]
+        [Required]
         public string PasswordHash { get; set; }
+
+        [Required]
         public int Salt { get; set; }
     }
 }
--- EnterTheDungeon/Migrations/20220423130831_Add column constraints.Designer.cs
+++ EnterTheDungeon/Migrations/20220423130831_Add column constraints.Designer.cs
@@ -3,14 +3,16 @@ using EnterTheDungeon.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EnterTheDungeon.Migrations
 {
     [DbContext(typeof(EnterTheDungeonDbContext))]
-    partial class EnterTheDungeonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220423130831_Add column constraints")]
+    partial class Addcolumnconstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
