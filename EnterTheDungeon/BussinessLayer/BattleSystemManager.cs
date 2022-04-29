d48063165a1e6675f16ae40a01b555aee5f780fe
kddinev18 <kddinev18@codingburgas.bg>
2022-04-29 21:59:05 +0300

    Add BattleSystemManager class

---
+++ EnterTheDungeon/BussinessLayer/BattleSystemManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterTheDungeon.BussinessLayer
+{
+    public static class BattleSystemManager
+    {
+        public static void InitiateBattle()
+        {
+
+        }
+    }
+}
